2414369028f2386408f0cd8a0cf88236a7200a70
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2022-08-20 09:41:17 -0400

    update

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+for (let i = 1; i <= 5; i++ ){
+    for(j = 1; j<=5; j++){
+        if (i ==1 || j==1|| i== 5){
+            document.write("x")
+        } else {
+            document.write(" &nbsp")
+        }
+    }
+    document.write("<br/>")
+}
